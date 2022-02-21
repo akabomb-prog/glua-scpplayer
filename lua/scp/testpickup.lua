49eb474735a57a4ec6068c04259025ce099db0dd
akabomb-prog <69766525+akabomb-prog@users.noreply.github.com>
2022-02-21 16:40:58 +0200

    Update testpickup.lua

--- lua/scp/testpickup.lua
+++ lua/scp/testpickup.lua
@@ -9,7 +9,8 @@ end
 
 local function DimensionalBust(ent)
     ent:SetModel(Model("models/props_combine/breenbust.mdl"))
-    ent:SetColor(HSLToColor(math.random(0, 6) * 60, 0.75, 0.75))
+    -- ent:SetColor(HSLToColor(math.random(0, 6) * 60, 0.75, 0.75))
+    ent:SetColor(Color(255, 255, 0, 255))
     ent:SetMaterial("models/screenspace")
     ent:SetCollisionGroup(COLLISION_GROUP_IN_VEHICLE)
     ent:SetVar("SCP_UseAnytime", true)
